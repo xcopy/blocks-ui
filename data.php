85d711fe5b576462450fa0591cd0d425f45c86bc
Kairat Jenishev <kairat.jenishev@gmail.com>
2020-10-27 18:55:40 +0600

    Render user's allowed blocks

--- data.php
+++ data.php
@@ -2,7 +2,8 @@
 
 return [
     'blocks' => [
-        1 => [
+        [
+            'id' => 1,
             'title' => 'Patient',
             'type' => 'success',
             'config' => [
@@ -11,15 +12,17 @@ return [
                 'prefer' => 7
             ]
         ],
-        2 => [
+        [
+            'id' => 2,
             'title' => 'Employment',
             'config' => [
                 'min' => 5,
-                'max' => 5,
+                'max' => 6,
                 'prefer' => 5
             ]
         ],
-        3 => [
+        [
+            'id' => 3,
             'title' => 'Benefits',
             'config' => [
                 'min' => 6,
@@ -29,12 +32,14 @@ return [
         ]
     ],
     'users' => [
-        1 => [
+        [
+            'id' => 1,
             'username' => 'john_doe',
             'email' => 'john.doe@gmail.com',
             'allowedBlocks' => [1, 2, 3]
         ],
-        2 => [
+        [
+            'id' => 2,
             'username' => 'jane_doe',
             'email' => 'jane.doe@gmail.com',
             'allowedBlocks' => [1, 3]
--- index.php
+++ index.php
@@ -3,6 +3,9 @@
 $config = require_once __DIR__.'/config.php';
 $data = require_once __DIR__.'/data.php';
 
+$totalBlocksCount = count($data['blocks']);
+$maxBlocks = $config['maxCols'] / $config['minCols']; // max blocks per row
+
 ?>
 
 <!doctype html>
@@ -20,18 +23,38 @@ $data = require_once __DIR__.'/data.php';
 </head>
 <body class="m-3">
 
+<?php foreach ($data['users'] as $user): ?>
+    <?php $userBlocksCount = count($user['allowedBlocks']); ?>
+
+    <h1 class="my-3">
+        <?php echo $user['username'] ?>
+    </h1>
+
     <div class="container-fluid">
         <div class="row">
-            <?php foreach ($data['blocks'] as $block): ?>
-                <div class="col-md-<?php echo $block['config']['prefer'] ?>">
-                    <div class="card mb-3">
-                        <div class="card-header text-uppercase"><?php echo $block['title'] ?></div>
-                        <div class="card-body"><?php echo $config['lorem'] ?></div>
+            <?php foreach ($data['blocks'] as $i => $block): ?>
+
+                <?php if (in_array($block['id'], $user['allowedBlocks'])): ?>
+
+                    <div class="col-md-<?php echo $block['config']['prefer'] ?>">
+                        <div class="card mb-3">
+                            <div class="card-header">
+                                <h5 class="text-uppercase d-inline-block mb-0"><?php echo $block['title'] ?></h5>
+                                <span>(col-md-<?php echo $block['config']['prefer'] ?>)</span>
+                            </div>
+                            <div class="card-body">
+                                <?php echo $config['lorem'] ?>
+                            </div>
+                        </div>
                     </div>
-                </div>
+
+                <?php endif; ?>
+
             <?php endforeach; ?>
         </div>
     </div>
+    <hr>
+<?php endforeach; ?>
 
 </body>
 </html>
