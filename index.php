2003234bc794931e2295d8717446ca20deb9ccf1
Kairat Jenishev <kairat.jenishev@gmail.com>
2020-10-27 22:04:24 +0600

    App structure reorganized

--- index.php
+++ index.php
@@ -1,75 +1,8 @@
 <?php
 
-$config = require_once __DIR__.'/config.php';
-$data = require_once __DIR__.'/data.php';
-
-function findBlock(int $id) {
-    global $data;
-
-    $i = array_search($id, array_column($data['blocks'], 'id'));
-
-    return $data['blocks'][$i];
-}
-
-function getTotalCols(array $user, string $key) {
-    $callback = function ($total, int $id) use ($key): int {
-        $block = findBlock($id);
-        $total += $block['cols'][$key];
-
-        return $total;
-    };
-
-    return array_reduce($user['blocks'], $callback, 0);
-}
-
-function getBlocks(array $user) {
-    global $data, $config;
-
-    $userBlocksCount = count($user['blocks']);
-    $totalBlocksCount = count($data['blocks']);
-
-    $blocks = array_map(
-        function (int $id): array {
-            $block = findBlock($id);
-            $cols = $block['cols']['prefer']; // by default
-
-            return compact('block', 'cols');
-        },
-        $user['blocks']
-    );
-
-    ['minCols' => $minCols, 'maxCols' => $maxCols] = $config;
-
-    $totalPreferCols = getTotalCols($user, 'prefer');
-    $totalMinCols = getTotalCols($user, 'min');
-
-    // var_dump($totalPreferCols, $totalMinCols);
-
-    if ($userBlocksCount < $totalBlocksCount) {
-        if ($totalPreferCols > $maxCols) {
-            if ($totalMinCols > $maxCols) {
-                // set the same width for each block
-                // all of them will be evenly spaced
-                $cols = ceil($maxCols / $userBlocksCount);
-                $cols = $cols < $minCols ? $minCols : $cols;
-
-                foreach ($blocks as &$arr) {
-                    $arr['cols'] = $cols;
-                }
-            } elseif ($totalMinCols < $maxCols) {
-                // ...
-            } else {
-                // set the minimum width for each block respectively
-                foreach ($blocks as &$arr) {
-                    ['block' => $block] = $arr;
-                    $arr['cols'] = $block['cols']['min'];
-                }
-            }
-        }
-    }
-
-    return $blocks;
-}
+$config = require_once __DIR__.'/src/config.php';
+$data = require_once __DIR__.'/src/data.php';
+require_once __DIR__.'/src/functions.php';
 
 ?>
 
@@ -78,7 +11,7 @@ function getBlocks(array $user) {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="./bootstrap.css">
+    <link rel="stylesheet" href="./css/bootstrap.css">
     <title>Blocks UI</title>
     <style>
         .card {
---
+++ src/functions.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * @param int $id
+ * @return array
+ */
+function findBlock(int $id): array {
+    global $data;
+
+    $i = array_search($id, array_column($data['blocks'], 'id'));
+
+    return $data['blocks'][$i];
+}
+
+/**
+ * @param array $user
+ * @param string $key
+ * @return int
+ */
+function getTotalCols(array $user, string $key): int {
+    $callback = function ($total, int $id) use ($key): int {
+        $block = findBlock($id);
+        $total += $block['cols'][$key];
+
+        return $total;
+    };
+
+    return array_reduce($user['blocks'], $callback, 0);
+}
+
+/**
+ * @param array $user
+ * @return array
+ */
+function getBlocks(array $user): array {
+    global $data, $config;
+
+    $userBlocksCount = count($user['blocks']);
+    $totalBlocksCount = count($data['blocks']);
+
+    $blocks = array_map(
+        function (int $id): array {
+            $block = findBlock($id);
+            $cols = $block['cols']['prefer']; // by default
+
+            return compact('block', 'cols');
+        },
+        $user['blocks']
+    );
+
+    ['minCols' => $minCols, 'maxCols' => $maxCols] = $config;
+
+    $totalPreferCols = getTotalCols($user, 'prefer');
+    $totalMinCols = getTotalCols($user, 'min');
+
+    // var_dump($totalPreferCols, $totalMinCols);
+
+    if ($userBlocksCount < $totalBlocksCount) {
+        if ($totalPreferCols > $maxCols) {
+            if ($totalMinCols > $maxCols) {
+                // set the same width for each block
+                // all of them will be evenly spaced
+                $cols = ceil($maxCols / $userBlocksCount);
+                $cols = $cols < $minCols ? $minCols : $cols;
+
+                foreach ($blocks as &$arr) {
+                    $arr['cols'] = $cols;
+                }
+            } elseif ($totalMinCols < $maxCols) {
+                // todo
+            } else {
+                // set the minimum width for each block respectively
+                foreach ($blocks as &$arr) {
+                    ['block' => $block] = $arr;
+                    $arr['cols'] = $block['cols']['min'];
+                }
+            }
+        }
+    }
+
+    return $blocks;
+}
